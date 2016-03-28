b0b3e2466055d4e0cecc32f606a2378bc568b114
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-03-28 08:09:46 -0700

    Fixing the order and coverage report for the unit tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 rm /tmp/dashed_unittests.db
+rm -f .coverage
 export DASHED_CONFIG=tests.dashed_test_config
 dashed/bin/dashed db upgrade
 python setup.py nosetests
+#python tests/core_tests.py
